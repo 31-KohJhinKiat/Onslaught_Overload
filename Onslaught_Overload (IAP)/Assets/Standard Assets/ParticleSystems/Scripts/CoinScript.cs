610824bd868f2b5020ec7addbd13231619631edd
JhinKiat <jhinkiat915@gmail.com>
2022-01-14 10:27:48 +0800

    need more assets

---
+++ Onslaught_Overload (IAP)/Assets/Standard Assets/ParticleSystems/Scripts/CoinScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CoinScript : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        //collision with coins
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+
+            GameManager.instance.addScore();
+            Destroy(this.gameObject);
+            print("score");
+        }
+    }
+}
---
+++ Onslaught_Overload (IAP)/Assets/Standard Assets/ParticleSystems/Scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    
+    private float scoreValue;
+    private float totalCoins;
+    public float timeLeft;
+    public int timeRemining;
+    
+    public Text ScoreText;
+    public Text TimerText;
+
+    private float TimerValue;
+
+    private AudioSource audioSource;
+    public AudioClip collectSound;
+
+    public bool pause;
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        totalCoins = 6;
+
+        audioSource = GetComponent<AudioSource>();
+
+        pause = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            pause = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        if (pause == true)
+        {
+
+            panel.SetActive(true);
+
+        }
+        else
+        {
+            panel.SetActive(false);
+            timeLeft -= Time.deltaTime;
+            timeRemining = Mathf.FloorToInt(timeLeft % 60);
+            TimerText.text = "Timer: " + timeRemining.ToString();
+        }
+
+        if(scoreValue == totalCoins)
+        {          
+          SceneManager.LoadScene("WinScene");
+            
+        }
+
+        else if (timeLeft <= 0)
+        {
+            SceneManager.LoadScene("LoseScene");
+
+        }
+
+    }
+
+    public void addScore()
+    {
+        audioSource.PlayOneShot(collectSound);
+        scoreValue++;
+        ScoreText.GetComponent<Text>().text = "Score: " + scoreValue;
+        
+    }
+
+    public void ResumeButton()
+    {
+        pause = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    }

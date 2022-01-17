986b63a3e63503e74e0c2e204708872092fdbcc6
Koh Jhin Kiat <jhinkiat915@gmail.com>
2022-01-17 12:46:42 +0800

    basic mouse look

--- Onslaught_Overload (IAP)/Assets/Scripts 2/PlayerScript.cs
+++ Onslaught_Overload (IAP)/Assets/Scripts 2/PlayerScript.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //player camera
+    Transform playerCamera = null;
+
+    //movement
     public float walkSpeed;
     public float runSpeed;
+
+    //jump
+    public Vector3 jump;
+    public float jumpForce = 3.0f;
+    public bool isGrounded;
+    Rigidbody rb;
+
+    //health
     public float playerHealth;
+
+    //gun
+
+
+
+    //animation
+    public Animator animator;
+
+    //sound
+    private AudioSource audioSource;
+    public AudioClip walkSound;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 3.0f, 0.0f);
+        //EndText.SetActive(false);
     }
 
     // Update is called once per frame
